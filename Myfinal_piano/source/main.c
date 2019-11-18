135c37b53597c9f1de6b7d538fbcee287ab35dc5
neha45556 <neha45556@gmail.com>
2019-11-18 14:33:36 -0800

    Update main.c

--- Myfinal_piano/source/main.c
+++ Myfinal_piano/source/main.c
@@ -12,12 +12,85 @@
 #include "/Users/nehagupta/Downloads/simavr-master/simavr/sim/avr/avr_mcu_section.h"
 #endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
-    DDRB = 0xFF; PORTB = 0x00;
-    /* Insert your solution below */
-    while (1) {
-	PORTB = 0x04;
+void set_PWM(double frequency){
+    static double current_frequency;
+    if(frequency != current_frequency){
+        if(!frequency){ TCCR3B &= 0x08; }
+        else{ TCCR3B |= 0x03; }
+        if(frequency < .954){ OCR3A = 0xFFFF;}
+        else if(frequency > 31250) { OCR3A = 0x0000; }
+        else { OCR3A = (short)(8000000 / (128* frequency)) -1; }
+        TCNT3 = 0;
+        current_frequency = frequency;
     }
-    return 1;
+}
+
+void PWM_on(){
+    TCCR3A = (1 << COM3A0);
+    TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+    set_PWM(0);
+}
+
+void PWM_off(){
+    TCCR3A = 0x00;
+    TCCR3B = 0x00;
+}
+
+#define PA0 (~PINA & 0x01)
+#define PA1 (~PINA & 0x02)
+#define buttons (~PINA & 0xFF)
+
+enum states {init, sound1, sound2} state;
+void sound(){
+	switch(state){
+		case init:
+			if(button == 0x01){
+				state = sound1;
+			}
+			if(button == 0x02){
+				state = sound2;
+			}
+			state = init; 
+			
+		break;
+		case sound1:
+			if(button == 0x01){
+				state = sound1;
+			}
+			else{
+				state = init;
+			}
+		break;
+// 		case sound2:
+// 			if(PA0){
+// 				state = sound1;
+// 			}
+// 			state = sound2;
+// 			break;
+	}
+	switch(state){
+		case init:
+			set_PWM(0);
+			break;
+		case sound1:
+			set_PWM(261.63);
+			break;
+		case sound2:
+			set_PWM(293.66);
+			break;
+	}
+				
+			
+}
+
+
+int main(void){
+  	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	PWM_on();
+	state = init;
+	while(1) {
+		sound();
+	}
+	return 1;
 }
