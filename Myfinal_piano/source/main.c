e3f2e732df0bd00df62c6b573b4e6a366ff4daee
neha45556 <neha45556@gmail.com>
2019-11-24 15:44:16 -0800

    Update main.c

--- Myfinal_piano/source/main.c
+++ Myfinal_piano/source/main.c
@@ -9,7 +9,7 @@
  */
 
 
-/* #include <avr/io.h> 
+ #include <avr/io.h> 
 #define F_CPU 8000000UL
 #include <util/delay.h>
 #include <Wire.h>
@@ -388,44 +388,10 @@ int main(void){
 }
 
 
-*/
+
 
 
   
-#include <Wire.h>
-void setup() {
-  Wire.begin();
-  Serial.begin(9600);
-  while (!Serial);
-  Serial.println("\nI2C Scanner");
-}
-void loop() {
-  byte error, address;
-  int nDevices;
-  Serial.println("Scanning...");
-  nDevices = 0;
-  for (address = 1; address < 127; address++ ) {
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-    if (error == 0) {
-      Serial.print("I2C device found at address 0x");
-      if (address < 16)
-        Serial.print("0");
-      Serial.print(address, HEX);
-      Serial.println("  !");
-      nDevices++;
-    }
-    else if (error == 4) {
-      Serial.print("Unknown error at address 0x");
-      if (address < 16)
-        Serial.print("0");
-      Serial.println(address, HEX);
-    }
-  }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found\n");
-  else
-    Serial.println("done\n");
-  delay(5000);
-}
+
+  
 
