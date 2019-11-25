6c063ce4ab4b678400661c350d340fe159426da8
neha45556 <neha45556@gmail.com>
2019-11-25 10:22:49 -0800

    Update main.c

--- Myfinal_piano/source/main.c
+++ Myfinal_piano/source/main.c
@@ -8,7 +8,7 @@
  *	code, is my own original work.
  */
 
-
+/*
 #include <avr/io.h> 
 #define F_CPU 8000000UL
 #include <util/delay.h>
@@ -388,6 +388,25 @@ int main(void){
 	}
 	return 1;
 }
+
+*/
+
+#include <avr/io.h>
+#include io.h
+
+int main(void){
+	DDRC = 0xFF; PORTC = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
+	
+	LCD_init();
+	LCD_DisplayString(1, "Hello World");
+	
+	while(1){continue;}
+	
+}
+	
+	
+
 
 
 
