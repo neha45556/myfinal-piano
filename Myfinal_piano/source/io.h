375e7003d6974d245291e7e14890ac4e962a66a6
neha45556 <neha45556@gmail.com>
2019-11-25 10:05:27 -0800

    Update io.h

--- Myfinal_piano/source/io.h
+++ Myfinal_piano/source/io.h
@@ -5,7 +5,7 @@ void LCD_init();
 void LCD_ClearScreen(void);
 void LCD_WriteCommand (unsigned char Command);
 void LCD_Cursor (unsigned char column);
-void LCD_DisplayString(unsigned char column , unsigned char *string);
+void LCD_DisplayString(unsigned char column , const unsigned char *string);
 //void LCD_Custom_Char (unsigned char loc, const unsigned char *msg);
 void delay_ms(int miliSec);
 #endif
